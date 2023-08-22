759e58b6cbf81435998c2b4b492faa2b1ed874dc
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-08-22 23:51:55 +0300

    entry point

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Мой топ фото</h1>
+            </header>
+            <p className="App-intro">
+                Здесь будут мои самые залайканые фото
+            </p>
+        </div>
+    );
+}
+
+export default App;
