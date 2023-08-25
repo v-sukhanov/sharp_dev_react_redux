4579728c38bf185e61346cd65a8920f47b4806ea
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-08-25 23:21:11 +0300

    entry point

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Product } from './components/Product';
+import { products } from './data/products';
+import axios from 'axios';
+import { IProduct } from './models';
 
 function App() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    async function fetchProducts() {
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+        setProducts(response.data)
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container mx-auto max-w-2xl pt-5">
+          {
+              products.map(product => <Product product={product}/>)
+          }
+
+      </div>
   );
 }
 
